46cb5ebe34ffd8a670d510f7afae801a363f4179
Vinicius Cavalcanti Nogueira de Sa <vini_cns2@hotmail.com>
2019-06-10 16:26:22 -0300

    Adicionado browsersync

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var SOURCEPATHS = {
+    sassSource: 'src/scss/*.scss'
+}
+
+var APPPATH = {
+    root: 'app/',
+    css: 'app/css',
+    js: 'app/js'
+}
+
+gulp.task('sass', function(){
+    return gulp.src(SOURCEPATHS.sassSource)
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(gulp.dest(APPPATH.css));
+});
+
+gulp.task('serve', function(){
+    browserSync.init([APPPATH.css+'/*.css', APPPATH.root + '*/.html', APPPATH.js + '/*.js'],{
+        server:{
+            baseDir: APPPATH.root
+        }
+    })
+});
+
+gulp.task('default', gulp.series('sass', 'serve'));

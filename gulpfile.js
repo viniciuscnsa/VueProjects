c81602f0774797f8835ac59b17627c011b8ca715
Vinicius Cavalcanti Nogueira de Sa <vini_cns2@hotmail.com>
2019-06-14 13:44:10 -0300

    Adicionado gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
 var SOURCEPATHS = {
@@ -16,15 +16,18 @@ var APPPATH = {
 gulp.task('sass', function(){
     return gulp.src(SOURCEPATHS.sassSource)
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(gulp.dest(APPPATH.css));
+        .pipe(gulp.dest(APPPATH.css))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('serve', function(){
-    browserSync.init([APPPATH.css+'/*.css', APPPATH.root + '*/.html', APPPATH.js + '/*.js'],{
+    browserSync.init([APPPATH.css+'/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'],{
         server:{
             baseDir: APPPATH.root
         }
     })
+    gulp.watch(SOURCEPATHS.sassSource, gulp.series('sass'));
+    gulp.watch(APPPATH.root + '/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.series('sass', 'serve'));
+gulp.task('default', gulp.series('serve'));

054b8e832ec3830964f9e1c63cb88cd7dc2a0ccb
Vinicius Cavalcanti Nogueira de Sa <vini_cns2@hotmail.com>
2019-06-20 18:27:25 -0300

    restruturado para gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,75 @@ var APPPATH = {
     js: 'app/js'
 }
 
+function cleanHtml (done){
+    gulp.src(APPPATH.root + '/*.html', {read: false, force: true})
+        .pipe(clean());
+    done();
+};
+
+function sassFiles (done){
+    gulp.src(SOURCEPATHS.sassSource)
+        .pipe(autoprefixer())   
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(gulp.dest(APPPATH.css))
+        .pipe(browserSync.stream());
+    done();
+};
+
+function copy (done){
+    gulp.src(SOURCEPATHS.htmlSource)
+        .pipe(gulp.dest(APPPATH.root));
+    done();
+};
+
+function serve(done){
+    browserSync.init([APPPATH.css+'/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'],{
+        server:{
+            baseDir: APPPATH.root
+        }
+    })
+    done();
+};
+
+function watchFiles(){
+    gulp.watch(SOURCEPATHS.sassSource, sassFiles);
+    gulp.watch(SOURCEPATHS.htmlSource, copy);
+    //gulp.series(sass, copy, cleanHtml, serve);
+};
+
+gulp.task("serve", serve);
+gulp.task("sassFiles", sassFiles);
+gulp.task("copy", copy);
+gulp.task("cleanHtml", cleanHtml);
+gulp.task("watchFiles", watchFiles);
+
+//default task for call all tasks
+gulp.task('default', gulp.series(watchFiles));
+
+/*
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+var autoprefixer = require('gulp-autoprefixer');
+var clean = require('gulp-clean');
+
+var SOURCEPATHS = {
+    sassSource: 'src/scss/*.scss',
+    htmlSource: 'src/*.html'
+}
+
+var APPPATH = {
+    root: 'app/',
+    css: 'app/css',
+    js: 'app/js'
+}
+
+gulp.task('clean-html', function(){
+    return gulp.src(APPPATH.root + '/*.html', {read: false, force: true})
+        .pipe(clean());
+})
+
 gulp.task('sass', function(){
     return gulp.src(SOURCEPATHS.sassSource)
         .pipe(autoprefixer())   
@@ -24,24 +93,28 @@ gulp.task('sass', function(){
         .pipe(browserSync.stream());
 });
 
-gulp.task('copy', function(){
+gulp.task('copy', done =>{
     gulp.src(SOURCEPATHS.htmlSource)
-        .pipe(gulp.dest(APPPATH.root))
+        .pipe(gulp.dest(APPPATH.root));
+    done();
 });
 
-gulp.task('clean-html', function(){
-    return gulp.src(APPPATH.root + '/*.html', {read: false, force: true})
-        .pipe(clean());
-})
-
-gulp.task('serve', function(){
+gulp.task('serve', done=>{
     browserSync.init([APPPATH.css+'/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'],{
         server:{
             baseDir: APPPATH.root
         }
     })
-    gulp.watch(SOURCEPATHS.sassSource, gulp.series('sass'));
-    gulp.watch(APPPATH.root + '/*.html').on('change', browserSync.reload);
+    done();
+    //gulp.watch(SOURCEPATHS.sassSource, gulp.series('sass'));
+    //gulp.watch(APPPATH.root + '/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.series(gulp.parallel('sass'), 'serve'));
+gulp.task('watch', gulp.series('serve', 'sass', 'copy', 'clean-html'), done=>{
+    gulp.watch(SOURCEPATHS.sassSource, gulp.series('sass'));
+    gulp.watch(SOURCEPATHS.htmlSource, gulp.series('copy'));
+    done();
+})
+
+gulp.task('default', gulp.series('watch'));
+*/

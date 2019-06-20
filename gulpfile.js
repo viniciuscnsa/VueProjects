28c9c5accf17930fb806c01554a46ecdc8c7a976
Vinicius Cavalcanti Nogueira de Sa <vini_cns2@hotmail.com>
2019-06-20 16:50:16 -0300

    clean html files in progress

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
+var autoprefixer = require('gulp-autoprefixer');
+var clean = require('gulp-clean');
 
 var SOURCEPATHS = {
-    sassSource: 'src/scss/*.scss'
+    sassSource: 'src/scss/*.scss',
+    htmlSource: 'src/*.html'
 }
 
 var APPPATH = {
@@ -15,10 +18,21 @@ var APPPATH = {
 
 gulp.task('sass', function(){
     return gulp.src(SOURCEPATHS.sassSource)
+        .pipe(autoprefixer())   
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         .pipe(gulp.dest(APPPATH.css))
         .pipe(browserSync.stream());
 });
+
+gulp.task('copy', function(){
+    gulp.src(SOURCEPATHS.htmlSource)
+        .pipe(gulp.dest(APPPATH.root))
+});
+
+gulp.task('clean-html', function(){
+    return gulp.src(APPPATH.root + '/*.html', {read: false, force: true})
+        .pipe(clean());
+})
 
 gulp.task('serve', function(){
     browserSync.init([APPPATH.css+'/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'],{
@@ -30,4 +44,4 @@ gulp.task('serve', function(){
     gulp.watch(APPPATH.root + '/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.series('serve'));
+gulp.task('default', gulp.series(gulp.parallel('sass'), 'serve'));
